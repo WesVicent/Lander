c2340242c0325b1a373bae446e6a8175b6623e6a
Wesley Ferreira <contato.wes.ferr@gmail.com>
2019-01-12 14:17:34 -0200

    Electron adjustments

--- source/electron/main.ts
+++ source/electron/main.ts
@@ -5,35 +5,31 @@
  * @Last Modified time: 2019-01-12 09:14:17
  */
 
-
 import { app, BrowserWindow, globalShortcut } from "electron";
 
 let win: BrowserWindow;
+// tslint:disable:object-literal-sort-keys
 
 function createWindow() {
-    // tslint:disable:object-literal-sort-keys
+    ////////////////// SETUP //////////////////
     win = new BrowserWindow({
         webPreferences: {
             nodeIntegration: false,
             webSecurity: false,
-
         },
+        show: false,
     });
+    win.maximize();
+    win.setMenu(null);
+
+    win.loadFile("./dist/index.html");
+    win.webContents.openDevTools();
 
     globalShortcut.register("f5", function () {
         win.reload();
     });
+    ///////////////////////////////////////////
 
-
-    win.setMenu(null);
-    win.maximize();
-
-    // and load the index.html of the app.
-    win.loadFile("./dist/index.html");
-
-    win.webContents.openDevTools();
-
-    // Emitted when the window is closed.
     win.on("closed", () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
@@ -42,9 +38,6 @@ function createWindow() {
     });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
 
 // Quit when all windows are closed.
@@ -63,6 +56,3 @@ app.on("activate", () => {
         createWindow();
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
